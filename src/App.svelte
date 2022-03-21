b6843a066b1e1f6f963d3dd3fe123b858de503f2
Glory Katende <katendeglory@gmail.com>
2022-03-21 11:03:28 +0200

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import QRCode from "./QRJS.svelte"
+</script>
+
+<h3>Eddie Florea - viper6277@gmail.com</h3>
+
+<h3>
+	Generate QR Codes with <a href="https://davidshimjs.github.io/qrcodejs/">QRcodeJS</a>
+</h3>
+
+<QRCode codeValue="12354234562345" squareSize=200/>
+
+
